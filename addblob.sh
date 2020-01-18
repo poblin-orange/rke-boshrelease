2ef41039b586aca5600c0623524e1c653ed36ad3
Pierre Oblin <pierre.oblin@orange.com>
2020-01-18 00:02:25 +0000

    bump rke 1.0.2

--- addblob.sh
+++ addblob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/rancher/rke/releases/download/v1.0.0/rke_linux-amd64
+wget https://github.com/rancher/rke/releases/download/v1.0.2/rke_linux-amd64
 bosh add-blob rke_linux-amd64 rke/rke
 #wget https://get.helm.sh/${HELM_FILE}
 #bosh add-blob ${HELM_FILE} helm/helm.tar.gz
