3f3d028247858f9cc820ec1582eb92ec7a974ada
Pierre Oblin <pierre.oblin@orange.com>
2020-01-26 18:17:07 +0000

    fix blobs

--- addblob.sh
+++ addblob.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-wget https://github.com/rancher/rke/releases/download/v1.0.2/rke_linux-amd64
-bosh add-blob rke_linux-amd64 rke/rke
-#wget https://get.helm.sh/${HELM_FILE}
-#bosh add-blob ${HELM_FILE} helm/helm.tar.gz
+wget https://github.com/rancher/rke/releases/download/v1.0.2/rke_linux-amd64 -o src/rke_linux-amd64
+bosh add-blob src/rke_linux-amd64 rke/rke
 
-rm rke_linux-amd64 
