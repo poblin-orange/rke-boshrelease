78ad5daa3be70a19f68b78dcf4ad0949a195c7a3
Pierre Oblin <pierre.oblin@orange.com>
2020-02-07 12:53:57 -0500

    add network plugin and options in spec

--- addblob.sh
+++ addblob.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+<<<<<<< HEAD
 set -x
 
 wget https://github.com/rancher/rke/releases/download/v1.0.2/rke_linux-amd64 -O src/rke_linux-amd64
+=======
+wget https://github.com/rancher/rke/releases/download/v1.0.4/rke_linux-amd64 -O src/rke_linux-amd64
+>>>>>>> fixup
 bosh add-blob src/rke_linux-amd64 rke/rke
 
