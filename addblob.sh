266df6919f6bbcc633f9821ad6106ebbfef8d4f2
Pierre Oblin <pierre.oblin@orange.com>
2020-07-03 09:18:35 +0000

    fix addblob script

--- addblob.sh
+++ addblob.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-wget https://github.com/rancher/rke/releases/download/v1.0.2/rke_linux-amd64 -o src/rke_linux-amd64
+set -x
+
+wget https://github.com/rancher/rke/releases/download/v1.0.2/rke_linux-amd64 -O src/rke_linux-amd64
 bosh add-blob src/rke_linux-amd64 rke/rke
 
